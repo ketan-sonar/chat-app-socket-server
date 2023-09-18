e7b6e9f47f5b5a1835bfaab4840f35cf2fb373df
Ketan Sonar <ketan.sonar.798@gmail.com>
2023-09-18 22:19:04 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import { Server } from "socket.io";
+
+const io = new Server(8900, { cors: { origin: "http://localhost:3000" } });
+
+let users: { userId: string; socketId: string }[] = [];
+
+const addUser = (userId: string, socketId: string) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId: userId, socketId: socketId });
+};
+
+const removeUser = (socketId: string) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId: string) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("addUser", (userId: string) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on(
+    "sendMessage",
+    ({
+      senderId,
+      receiverId,
+      text,
+      timestamp,
+    }: {
+      senderId: string;
+      receiverId: string;
+      text: string;
+      timestamp: Date;
+    }) => {
+      const receiver = getUser(receiverId);
+      io.to(receiver?.socketId as string).emit("getMessage", {
+        senderId,
+        text,
+        timestamp,
+      });
+    }
+  );
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
